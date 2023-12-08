31bc35d736032ff0e71dc073eb0700c130e065a6
hiibro133 <151934679+hiibro133@users.noreply.github.com>
2023-12-08 14:14:02 +0530

    Update Banner.tsx

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -29,6 +29,7 @@ function Banner({ netflixOriginals }: Props) {
   const [currentMovie, setCurrentMovie] = useRecoilState(movieState)
   const [trailer, setTrailer] = useState('')
   const [muted, setMuted] = useState(true)
+    const dc = movie?.overview;
 
 
   useEffect(() => {
@@ -101,14 +102,14 @@ function Banner({ netflixOriginals }: Props) {
       <h1 className="text-2xl font-bold md:text-4xl lg:text-7xl">
         {movie?.title || movie?.name || movie?.original_name}
       </h1>
-      <p className="max-w-xs text-xs text-shadow-md md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl">
-        {movie?.overview}
-      </p>
+      <div className="max-w-xs text-xs text-shadow-md md:max-w-lg md:text-lg lg:max-w-1xl lg:text-2xl text-white-100" style={{ textDecoration: 'none' }} >
+        <ReadMore text={dc} wordsLimit={5} />
+      </div>
 
       <div className="flex space-x-3">
-        <button className="bannerButton bg-white text-black">
+        {/*<button className="bannerButton bg-white text-black">
           <FaPlay className="h-4 w-4 text-black md:h-7 md:w-7" /> Play
-        </button>
+        </button>*/}
         <button
           className="bannerButton bg-[gray]/70"
           onClick={() => {

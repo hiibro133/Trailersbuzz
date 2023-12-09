fc0a4fcfa1e1631d3a4d3aa1bbd2f9433ad2c83b
hiibro133 <151934679+hiibro133@users.noreply.github.com>
2023-12-09 16:03:45 +0530

    Update a-ads2.tsx

--- components/a-ads2.tsx
+++ components/a-ads2.tsx
@@ -1,11 +1,11 @@
 // AdComponent.js
 import React from 'react';
-
+//<iframe data-aa='2284222' src='//ad.a-ads.com/2284222?size=120x60' style='width:120px; height:60px; border:0px; padding:0; overflow:hidden; background-color: transparent;'></iframe>
 const AdComponent2 = () => (
   <iframe
-    data-aa='2283722'
-    src='ad.a-ads.com/2283722?size=728x90'
-    style={{ width: '728px', height: '90px', border: '0', padding: '0', overflow: 'hidden', backgroundColor: 'transparent' }}
+    data-aa='2284222'//'2283722'
+    src='//ad.a-ads.com/2284222?size=120x60'//'ad.a-ads.com/2283722?size=728x90'
+    style= {{ width: '120px', height: '60px', border: '0', padding: '0', overflow: 'hidden', backgroundColor: 'transparent' }} //{{ width: '728px', height: '90px', border: '0', padding: '0', overflow: 'hidden', backgroundColor: 'transparent' }}
   ></iframe>
 );
 

0f41d1a2b4391f9379d4847510c47c3ee07dbbe8
hiibro133 <151934679+hiibro133@users.noreply.github.com>
2023-12-09 16:41:58 +0530

    Update a-ads3.tsx

--- components/a-ads3.tsx
+++ components/a-ads3.tsx
@@ -1,1 +1,13 @@
 //<iframe data-aa='2284229' src='//ad.a-ads.com/2284229?size=120x60' style='width:120px; height:60px; border:0px; padding:0; overflow:hidden; background-color: transparent;'></iframe>
+// AdComponent.js
+import React from 'react';
+
+const ads3 = () => (
+  <iframe
+    data-aa='2284229'
+    src= '//ad.a-ads.com/2284229?size=120x60'
+    style={{ width: '120px', height: '60px', border: '0', padding: '0', overflow: 'hidden', backgroundColor: 'transparent' }}
+  ></iframe>
+);
+
+export default ads3;

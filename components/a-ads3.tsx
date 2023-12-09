dc456d1cd9595c229ad1a7d75f75fcd3e63ed30b
hiibro133 <151934679+hiibro133@users.noreply.github.com>
2023-12-09 16:40:32 +0530

    Create a-ads3.tsx

---
+++ components/a-ads3.tsx
@@ -0,0 +1,1 @@
+//<iframe data-aa='2284229' src='//ad.a-ads.com/2284229?size=120x60' style='width:120px; height:60px; border:0px; padding:0; overflow:hidden; background-color: transparent;'></iframe>

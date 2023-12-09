9867426b7145fa3db9af5f6bbbab204bbe53c9bc
hiibro133 <151934679+hiibro133@users.noreply.github.com>
2023-12-09 16:18:29 +0530

    Create BannerAds-a.tsx

---
+++ components/BannerAds-a.tsx
@@ -0,0 +1,5 @@
+// <iframe
+    data-aa='2284222'//'2283722'
+    src='//ad.a-ads.com/2284222?size=120x60'//'ad.a-ads.com/2283722?size=728x90'
+    style= {{ width: '120px', height: '60px', border: '0', padding: '0', overflow: 'hidden', backgroundColor: 'transparent' }} //{{ width: '728px', height: '90px', border: '0', padding: '0', overflow: 'hidden', backgroundColor: 'transparent' }}
+  ></iframe>

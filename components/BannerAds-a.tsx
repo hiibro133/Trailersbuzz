96841641b43003fd657ffb619e5b0c7fad9690ee
hiibro133 <151934679+hiibro133@users.noreply.github.com>
2023-12-09 16:24:47 +0530

    Update BannerAds-a.tsx

--- components/BannerAds-a.tsx
+++ components/BannerAds-a.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 const BannerAdsa = () => (
   <iframe
     data-aa='2284223'//'2284222'//'2283722'
-    src='//ad.a-ads.com/2284223?size=728x90''//'//ad.a-ads.com/2284222?size=120x60'//'ad.a-ads.com/2283722?size=728x90'
+    src='//ad.a-ads.com/2284223?size=728x90'//'//ad.a-ads.com/2284222?size=120x60'//'ad.a-ads.com/2283722?size=728x90'
     style= {{ width: '728px', height: '90px', border: '0', padding: '0', overflow: 'hidden', backgroundColor: 'transparent' }} //{{ width: '728px', height: '90px', border: '0', padding: '0', overflow: 'hidden', backgroundColor: 'transparent' }}
   ></iframe>
 );
